aeb40fc602c8db1f15e744c6626b3fa0a420cc53
Dashamir Hoxha <dashohoxha@gmail.com>
2016-03-08 22:11:53 +0100

    Fix the restart util.

--- utils/restart-gpg-agent.sh
+++ utils/restart-gpg-agent.sh
@@ -2,5 +2,8 @@
 
 killall gpg-agent
 rm -rf /tmp/gpg-*
+rm -f $EGPG_DIR/.gpg-agent-info
+unset GPG_AGENT_INFO
+unset GPG_TTY
 echo "Start gpg-agent by reloading ~/.bashrc:"
 echo "  source ~/.bashrc"
