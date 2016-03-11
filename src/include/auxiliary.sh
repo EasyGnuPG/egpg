e19de6028f3d3e7c0c0360752c0028e1cb5efffb
Dashamir Hoxha <dashohoxha@gmail.com>
2016-03-11 21:52:36 +0100

    Split functions into several include files.

---
+++ src/include/auxiliary.sh
@@ -0,0 +1,114 @@
+#!/usr/bin/env bash
+#
+# EasyGnuPG is a wrapper around GnuPG to simplify its operations.
+# Copyright (C) 2016  Dashamir Hoxha
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/
+
+#
+# BEGIN helper functions
+#
+
+# Return the ids of the keys that are not revoked and not expired.
+get_valid_keys(){
+    local homedir="${1:-$GNUPGHOME}"
+    local valid_keys=''
+    local secret_keys key_id keyinfo expiration
+    secret_keys=$(gpg --homedir="$homedir" --list-secret-keys --with-colons | grep '^sec' | cut -d: -f5)
+    for key_id in $secret_keys; do
+        keyinfo=$(gpg --homedir="$homedir" --list-keys --with-colons $key_id | grep '^pub:u:')
+        [[ -z $keyinfo ]] && continue
+        expiration=$(echo "$keyinfo" | cut -d: -f7)
+        [[ -n $expiration ]] && [[ $expiration -lt $(date +%s) ]] && continue
+        valid_keys+=" $key_id"
+    done
+    echo $valid_keys
+}
+
+get_gpg_key(){
+    [[ -z $GPG_KEY ]] || return
+    GPG_KEY=$(get_valid_keys | cut -d' ' -f1)
+    [[ -z $GPG_KEY ]] && fail "No valid key found.\nTry first:  $0 key gen"
+
+    # check for key expiration
+    # an expired key can be renewed at any time, so only a warning is issued
+    local key_details exp
+    key_details=$(gpg --list-keys --with-colons $GPG_KEY)
+    key_ids=$(echo "$key_details" | grep -E '^pub|^sub' | cut -d: -f5)
+    for key_id in $key_ids; do
+        exp=$(echo "$key_details" | grep -E ":$key_id:" | cut -d: -f7)
+        if [[ $exp -lt $(date +%s) ]]; then
+            echo -e "\nThe key $key_id has expired on $(date -d @$exp +%F).\nPlease renew it with:  $0 key renew\n"
+            break
+        elif [[ $(($exp - $(date +%s))) -lt $((2*24*60*60)) ]]; then
+            echo -e "\nThe key $key_id is expiring soon.\nPlease renew it with:  $0 key renew\n"
+            break
+        fi
+    done
+}
+
+# Fail with a suitable message if there is any valid key.
+# This is called before generating or importing a new key,
+# to make sure that there is no more than one valid key.
+assert_no_valid_key(){
+    local gpg_key=$(get_valid_keys | cut -d' ' -f1)
+    [[ -z $gpg_key ]] || fail "There is already a valid key.\nRevoke or delete it first."
+}
+
+gpg_send_keys() {
+    [[ -z $SHARE ]] && return
+    gpg --keyserver "$KEYSERVER" --send-keys "$@"
+}
+
+get_new_passphrase() {
+    local passphrase passphrase_again
+    while true; do
+        read -r -p "Enter passphrase for the new key: " -s passphrase || return
+        echo
+        read -r -p "Retype the passphrase of the key: " -s passphrase_again || return
+        echo
+        if [[ "$passphrase" == "$passphrase_again" ]]; then
+            PASSPHRASE="$passphrase"
+            break
+        else
+            echo "Error: the entered passphrases do not match."
+        fi
+    done
+}
+
+get_passphrase() {
+    [[ -v PASSPHRASE ]] && return
+    read -r -p "Passphrase: " -s PASSPHRASE || return
+    [[ -t 0 ]] && echo
+}
+
+yesno() {
+    local response
+    read -r -p "$1 [y/N] " response
+    [[ $response == [yY] ]] || return 1
+}
+
+fail() {
+    echo -e "$@" >&2
+    exit 1
+}
+
+debug() {
+    [[ -z $DEBUG ]] && return
+    echo "$@"
+}
+
+#
+# END helper functions
+#
---
+++ src/include/platform.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+#
+# EasyGnuPG is a wrapper around GnuPG to simplify its operations.
+# Copyright (C) 2016  Dashamir Hoxha
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/
+
+#
+# BEGIN platform definable
+#
+
+gpg() { "$(which gpg2)" $GPG_OPTS "$@" ; }
+export -f gpg
+
+getopt() { "$(which getopt)" "$@" ; }
+export -f getopt
+
+shred() { "$(which shred)" -f -z -u "$@" ; }
+export -f shred
+
+haveged_start() {
+    [[ -z "$(ps ax | grep -v grep | grep haveged)" ]] || return
+    echo "
+Starting haveged which will greatly improve the speed of creating
+a new key, by improving the entropy generation of the system."
+    sudo haveged -w 1024
+    echo
+    HAVEGED_STARTED="true"
+}
+
+haveged_stop() {
+    [[ -z $HAVEGED_STARTED ]] && return
+    sudo killall haveged
+}
+
+make_workdir() {
+    local warn=1
+    [[ $1 == "nowarn" ]] && warn=0
+    local template="$PROGRAM.XXXXXXXXXXXXX"
+    if [[ -d /dev/shm && -w /dev/shm && -x /dev/shm ]]; then
+        WORKDIR="$(mktemp -d "/dev/shm/$template")"
+        remove_tmpfile() {
+            rm -rf "$WORKDIR"
+        }
+        trap remove_tmpfile INT TERM EXIT
+    else
+        if [[ $warn -eq 1 ]]; then
+            yesno "$(cat <<- _EOF
+Your system does not have /dev/shm, which means that it may
+be difficult to entirely erase the temporary non-encrypted
+password file after editing.
+
+Are you sure you would like to continue?
+_EOF
+                    )" || return
+        fi
+        WORKDIR="$(mktemp -d "${TMPDIR:-/tmp}/$template")"
+        shred_tmpfile() {
+            find "$WORKDIR" -type f -exec shred {} +
+            rm -rf "$WORKDIR"
+        }
+        trap shred_tmpfile INT TERM EXIT
+    fi
+}
+
+#
+# END platform definable
+#
