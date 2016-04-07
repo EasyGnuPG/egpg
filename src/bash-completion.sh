1e9d844de9202412ca5dbaa55e69b67860948c04
Dashamir Hoxha <dashohoxha@gmail.com>
2016-04-07 11:18:00 +0200

    Completion for the command open, plus some small changes.

--- src/bash-completion.sh
+++ src/bash-completion.sh
@@ -7,10 +7,11 @@
 _egpg_complete_dir() {
     local default="$1"
     local pattern="${cur:-$default}"
+    COMPREPLY=()
     pattern="${pattern%/}*"
     for dir in $pattern ; do
         [[ -d "$dir" ]] || continue
-        COMPREPLY+=("$dir")
+        COMPREPLY+=( "$dir" )
     done
     [[ -z "${COMPREPLY[@]}" ]] && COMPREPLY+=("$default")
 }
@@ -21,13 +22,13 @@ _egpg()
     local cur=$2
     if [[ $COMP_CWORD == 1 ]]; then
         local commands="init migrate info seal open sign verify set key contact gpg help version"
-        COMPREPLY+=($(compgen -W "$commands" -- $cur))
+        COMPREPLY=( $(compgen -W "$commands" -- $cur) )
         return
     fi
 
-    local first="${COMP_WORDS[1]}"
+    local cmd="${COMP_WORDS[1]}"
     local last=$3
-    case $first in
+    case $cmd in
         init)
             _egpg_complete_dir ~/.egpg
             ;;
@@ -35,15 +36,20 @@ _egpg()
             if [[ $last == "-d" || $last == "--homedir" ]]; then
                 _egpg_complete_dir ~/.gnupg
             else
-                COMPREPLY+=($(compgen -W "-d --homedir" -- $cur))
+                COMPREPLY=( $(compgen -W "-d --homedir" -- $cur) )
             fi
             ;;
         seal)
             if [[ $COMP_CWORD == 2 ]]; then
-                COMPREPLY+=($(compgen -f -- $cur))
+                COMPREPLY=( $(compgen -f -- $cur) )
             else
                 local contacts=$(egpg contact ls | grep '^uid: ' | cut -d"<" -f2 | cut -d">" -f1)
-                COMPREPLY+=($(compgen -W "$contacts" -- $cur))
+                COMPREPLY=( $(compgen -W "$contacts" -- $cur) )
+            fi
+            ;;
+        open)
+            if [[ $last == 'open' ]]; then
+                COMPREPLY=( $(compgen -f -X '!*.sealed' -- $cur) )
             fi
             ;;
     esac
