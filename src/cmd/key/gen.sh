d77b803f9cce5681c3782bd6ecb02352ba1d339f
Dashamir Hoxha <dashohoxha@gmail.com>
2016-04-15 21:54:30 +0200

    Update cmd_key_gen

--- src/cmd/key/gen.sh
+++ src/cmd/key/gen.sh
@@ -2,9 +2,16 @@
 
 cmd_key_gen_help() {
     cat <<-_EOF
-    gen,generate [<email> <name>] [-n,--no-passphrase]
+    gen,generate [<email> <name>] [-u,--unsplit] [-p,--passphrase] \\
+                 [-d,--dongledir <dir>] [-b,--backupdir <dir>]
         Create a new GPG key. If <email> and <name> are not given as
-        arguments, they will be asked interactively.
+        arguments, they will be asked interactively. The same for the
+        --dongledir option. The default for the --backupdir option is
+        the current working directory ($(pwd)).
+        By default the key is split into three partial keys (one
+        stored on the dongle, one locally, and one for backup) and no
+        passphrase is used.  These can be changed with the options
+        --unsplit and --passphrase.
 
 _EOF
 }
@@ -12,19 +19,24 @@ _EOF
 cmd_key_gen() {
     assert_no_valid_key
 
-    local opts pass=1
-    opts="$(getopt -o n -l no-passphrase -n "$PROGRAM" -- "$@")"
+    local opts split=1 pass dongledir backupdir=$(pwd)
+    opts="$(getopt -o upd:b: -l unsplit,passphrase,dongledir:,backupdir: -n "$PROGRAM" -- "$@")"
     local err=$?
     eval set -- "$opts"
     while true; do
         case $1 in
-            -n|--no-passphrase) pass=0; shift ;;
+            -u|--unsplit) split=0; shift ;;
+            -p|--passphrase) pass=1; shift ;;
+            -d|--dongledir) dongledir="$2"; shift 2 ;;
+            -b|--backupdir) backupdir="$2"; shift 2 ;;
             --) shift; break ;;
         esac
     done
-    [[ $err -ne 0 ]] && fail "Usage:\n$(cmd_key_gen_help)"
+    [[ $err == 0 ]] || fail "Usage:\n$(cmd_key_gen_help)"
 
     local email=$1 real_name=$2
+    [[ -z $pass && $split == 0 ]] && pass=1
+    [[ -z $pass ]] && pass=0
 
     echo -e "\nCreating a new key.\n"
 
@@ -36,7 +48,28 @@ cmd_key_gen() {
     [[ -n "$real_name" ]] || read -e -p "Real Name to be associated with the key: " real_name
     real_name=${real_name:-anonymous}
 
-    haveged_start
+    if [[ $split == 1 ]]; then
+        # get the dongle dir
+        if [[ -z "$dongledir" ]]; then
+            local guess suggest
+            guess="$DONGLE"
+            [[ -z "$guess" ]] && guess=$(df -h | grep '/dev/sdb1' | sed 's/ \+/:/g' | cut -d: -f6)
+            [[ -z "$guess" ]] && guess=$(df -h | grep '/dev/sdc1' | sed 's/ \+/:/g' | cut -d: -f6)
+            [[ -n "$guess" ]] && suggest=" [$guess]"
+            echo
+            read -e -p "Enter the dongle directory$suggest: " dongledir
+            echo
+            dongledir=${dongledir:-$guess}
+        fi
+        [[ -n "$dongledir" ]] || fail "You need a dongle to save the partial key."
+        [[ -d "$dongledir" ]] || fail "Dongle directory does not exist: $dongledir"
+        [[ -w "$dongledir" ]] || fail "Dongle directory is not writable: $dongledir"
+        dongledir=${dongledir%/}
+
+        # check the backup dir
+        [[ -d "$backupdir" ]] || fail "Backup directory does not exist: $backupdir"
+        [[ -w "$backupdir" ]] || fail "Backup directory is not writable: $backupdir"
+    fi
 
     local PARAMETERS="
         Key-Type: RSA
@@ -56,10 +89,11 @@ cmd_key_gen() {
     else
         PASSPHRASE=''
     fi
+
+    # generate the key
+    haveged_start
     gpg --quiet --batch --gen-key <<< "$PARAMETERS"
-
-    [[ $? -ne 0 ]] && return 1
-
+    [[ $? != 0 ]] && fail "Failed to generate a key."
     # set up some sub keys, in order not to use the base key day-to-day
     get_gpg_key
     local commands="addkey|4|4096|1m|addkey|6|4096|1m|save"
@@ -68,8 +102,18 @@ cmd_key_gen() {
     while [[ -n $(ps ax | grep -e '--edit-key' | grep -v grep) ]]; do sleep 0.5; done
     haveged_stop
 
+    # split the key into partial keys
+    if [[ $split == 1 ]]; then
+        local options=''
+        [[ -n $dongledir ]] && options+=" -d $dongledir"
+        [[ -n $backupdir ]] && options+=" -b $backupdir"
+        call cmd_key_split $options
+    fi
+
     echo -e "\nExcellent! You created a fresh GPG key. Here's what it looks like:"
     call cmd_key_list
+
+    return 0
 
     # generate a revokation certificate
     call cmd_key_revcert "This revocation certificate was generated when the key was created."
