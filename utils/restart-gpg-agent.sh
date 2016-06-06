a747bdcc0b89bf2e209110d12763c144395e4f9a
Dashamir Hoxha <dashohoxha@gmail.com>
2016-06-06 20:00:30 +0200

    Update util reload-gpg-agent

--- utils/restart-gpg-agent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-killall gpg-agent
-rm -rf /tmp/gpg-*
-rm -f $EGPG_DIR/.gpg-agent-info
-unset GPG_AGENT_INFO
-unset GPG_TTY
-echo "Start gpg-agent by reloading ~/.bashrc:"
-echo "  source ~/.bashrc"
