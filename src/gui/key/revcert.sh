4a23760d90d39f3060ee5b2fa6e4b4e267312568
Divesh Uttamchandani <diveshuttamchandani@gmail.com>
2018-08-23 23:42:27 +0530

    added gui revcert
    helps with #88

--- src/gui/key/revcert.sh
+++ src/gui/key/revcert.sh
@@ -1,5 +1,24 @@
 gui_key_revcert(){
-    message error "<tt> ${FUNCNAME[0]}  \n not implemented yet </tt>"
+    description=$(yad --title="EasyGnuPG | Revcert" \
+        --text="Generate revcert for $GPG_KEY" \
+        --form \
+        --columns=2 \
+        --field="Description" \
+        --button=gtk-yes \
+        --button=gtk-quit \
+        --borders=10 | cut -d'|' -f1)
+    output=$(call cmd_key_revcert $description)
+
+    err=$?
+    is_true $DEBUG && echo "$output"
+
+    if [[ $err == 0 ]]; then
+        message info "Revocation certificate generated successfully"
+    else
+        fail_details=$(echo "$output" | grep '^gpg:' | uniq | pango_raw)
+        message error "Failed to generate revocation certificate for $GPG_KEY.\n <tt>$fail_details</tt>" 
+        return 1
+    fi
 }
 #
 # This file is part of EasyGnuPG.  EasyGnuPG is a wrapper around GnuPG
